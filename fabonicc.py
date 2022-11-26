a2fd1cd81f114dca40f63780a6455e5836e2e319
Aditya4769 <114800003+Aditya4769@users.noreply.github.com>
2022-11-26 19:46:37 +0530

    Fabonicc

--- fabonicc.py
+++ fabonicc.py
@@ -1,17 +1,13 @@
-# Taking Number Input From User
-number = int(input("Enter the number: "))
+# PROGRAM TO CHECK IF A NUMBER IS IN FIBONACCI SERIES OR NOT
 
-# Defining a Function Name as fibonacci
+# defining a function to check if the given number is in fibonacci series or not 
 
 def fibonacci(num):
-
 # Taking x=0 and y=1
     x = 0
     y = 1
     if num == 0 or num == 1 :
         return 1
-# Code for fibonacci series
-   
     while y <= num :
         z = x + y
         x = y
@@ -20,10 +16,19 @@ def fibonacci(num):
             return 1
             break
 
-# Putting condition for checking the given number are in fibonacci series or not
+# taking input from the user
 
-if fibonacci(number) == 1 :
-    print("The given number is in Fibonacci Series: ",number)
-else :
-    print("The given number is not in Fibonacci series: ",number)
+a = 1
+while a == 1:
+    number = int(input("Enter the number: "))
 
+# Calling the function to check if the given number is in fibonacci series or not
+
+    if fibonacci(number) == 1 :
+        print("The given number is in Fibonacci Series: ",number)
+    else :
+        print("The given number is not in Fibonacci series: ",number)
+
+# Checking if the user wants to check for more number
+
+    x = int(input("Enter 1 if you want to check for more number: "))
